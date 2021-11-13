8637a27cfdc20d625b8f86105f4428136bce4a69
wilson <305066231@qq.com>
2021-11-13 12:57:32 +0800

    add catchLatestVersionFromGithub and test fix parseBranchByTag test

--- src/main/java/fun/hydd/cddabrowser/utils/VersionUtil.java
+++ src/main/java/fun/hydd/cddabrowser/utils/VersionUtil.java
@@ -1,11 +1,43 @@
 package fun.hydd.cddabrowser.utils;
 
+import fun.hydd.cddabrowser.MainVerticle;
 import fun.hydd.cddabrowser.entity.Version;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.RequestOptions;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.regex.Pattern;
 
 public class VersionUtil {
+  public static final String GITHUB_RELEASES_URL = "https://api.github.com/repos/CleverRaven/Cataclysm-DDA/releases";
+  private static final Logger logger = LoggerFactory.getLogger(VersionUtil.class);
+
   private VersionUtil() {
+  }
+
+  public static Future<Version> catchLatestVersionFromGithub(Vertx vertx) {
+    return HttpUtil.request(vertx,
+        new RequestOptions()
+          .setAbsoluteURI(GITHUB_RELEASES_URL)
+          .putHeader("User-Agent", MainVerticle.PROJECT_NAME)
+          .setSsl(true))
+      .compose(buffer -> {
+        final JsonArray jsonArray = buffer.toJsonArray();
+        if (jsonArray.isEmpty()) {
+          return Future.failedFuture("catchLatestVersion(),jsonArray is Empty");
+        }
+        final JsonObject jsonObject = jsonArray.getJsonObject(0);
+        final Version version = jsonObject.mapTo(Version.class);
+        version.setBranch(parseBranchByTag(version.getTagName()));
+        logger.info("catchLatestVersion:\n" +
+          "{}", Json.encodePrettily(version));
+        return Future.succeededFuture(version);
+      });
   }
 
   public static int parseBranchByTag(final String tag) {
--- src/test/java/fun/hydd/cddabrowser/utils/VersionUtilTest.java
+++ src/test/java/fun/hydd/cddabrowser/utils/VersionUtilTest.java
@@ -1,13 +1,20 @@
 package fun.hydd.cddabrowser.utils;
 
 import fun.hydd.cddabrowser.entity.Version;
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(VertxExtension.class)
 class VersionUtilTest {
 
+  @Test
   void parseBranchByTag() {
-    String tag = "0.F-2 ";
+    String tag = "0.F-2";
     String tag1 = "0.F";
     String tag2 = "cdda-experimental-2021-10-10-1829";
     String tag3 = "cdda-experimental-2021-5-10-1829";
@@ -15,6 +22,16 @@ class VersionUtilTest {
     assertThat(VersionUtil.parseBranchByTag(tag1)).isEqualTo(Version.STABLE);
     assertThat(VersionUtil.parseBranchByTag(tag2)).isEqualTo(Version.EXPERIMENTAL);
     assertThat(VersionUtil.parseBranchByTag(tag3)).isEqualTo(Version.EXPERIMENTAL);
+  }
+
+  @Test
+  void catchLatestVersionFromGithub(Vertx vertx, VertxTestContext testContext) {
+    VersionUtil.catchLatestVersionFromGithub(vertx)
+      .onComplete(testContext.succeeding(version -> {
+        assertThat(version).isNotNull();
+        assertThat(version.getTagName()).isNotBlank();
+        testContext.completeNow();
+      }));
   }
 
 }
